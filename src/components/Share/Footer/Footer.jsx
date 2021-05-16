3e7c2dfd03a96f159d88b117460b007bae6c8976
misbah Hasan <tahermisbah41@gmail.com>
2021-05-17 01:59:00 +0600

    error fixed

--- src/components/Share/Footer/Footer.jsx
+++ src/components/Share/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import FooterContact from "./FooterContact/FooterContact";
 import FooterFeeds from "./FooterFeeds/FooterFeeds";
